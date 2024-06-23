4938d769d7c11584dd448d5d89ecc675443d0ef5
nicoanibarro <anibarro.nico@gmail.com>
2024-06-23 03:10:13 -0300

    Modificacion create.php

--- create.php
+++ create.php
@@ -1,29 +1,26 @@
 <?php
 
-    include("database.php");
+include("database.php");
 
-    echo "conexion exitosa";
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-    if (isset($_POST['submit'])) {
+    $nombre = $_POST['nombreInput'];
+    $apellido = $_POST['apellidoInput'];
+    $dni = $_POST['dniInput'];
+    $cuil = $_POST['cuilInput'];
+    $mail = $_POST['emailInput'];
+    $tipo_empleado_idtipo_empleado = $_POST['tipoEmpleadoSelect'];
+    $historial_academico_idhistorial_academico = $_POST['historialAcademicoSelect'];
+    $dato_laboral_iddato_laboral = $_POST['datoLaboralSelect'];
 
-        $nombre = $_POST['nombreInput'];
-        $apellido = $_POST['apellidoInput'];
-        $dni = $_POST['dniInput'];
-        $cuil = $_POST['cuilInput'];
-        $mail = $_POST['mailInput'];
-        $tipo_empleado_idtipo_empleado = $_POST['tipoEmpleadoSelect'];
-        $historial_academico_idhistorial_academico = $_POST['historialAcademicoSelect'];
-        $dato_laboral_iddato_laboral = $_POST['datoLaboralSelect'];
+    $sql = "INSERT INTO empleado (nombre, apellido, dni, cuil, mail, tipo_empleado_idtipo_empleado, historial_academico_idhistorial_academico, dato_laboral_iddato_laboral) VALUES ('$nombre', '$apellido', '$dni', '$cuil', '$mail', '$tipo_empleado_idtipo_empleado', '$historial_academico_idhistorial_academico', '$dato_laboral_iddato_laboral')";
 
-        $sql = "INSERT INTO empleado (nombre, apellido, dni, cuil, mail, tipo_empleado_idtipo_empleado, historial_academico_idhistorial_academico, dato_laboral_iddato_laboral) VALUES ('$nombre', '$apellido', '$dni', '$cuil', '$mail', '$tipo_empleado_idtipo_empleado', '$historial_academico_idhistorial_academico', '$dato_laboral_iddato_laboral')";
+    $result = mysqli_query($conn, $sql);
 
-        $result = mysqli_query($conn,$sql);
+    if (!$result) {
+        die('Query failed.');
+    }
 
-        if(!$result){
-            die('Query failed.');
-        }
-
-        echo 'Empleado Cargado';
+    echo 'Empleado Cargado';
 }
-
 ?>
