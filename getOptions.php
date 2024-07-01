3a5adc3cf1525f33e0c399bbecb5e4570a28fe59
flakanno <flakanno@hotmail.com>
2024-07-01 19:37:31 -0300

    creacion getoption and modified index

---
+++ getOptions.php
@@ -0,0 +1,17 @@
+<?php
+include('db.php'); // Incluye el archivo de conexión a la base de datos
+
+$table = $_GET['table'];
+$idField = $_GET['idField'];
+$descField = $_GET['descField'];
+
+$options = array();
+
+$sql = "SELECT $idField, $descField FROM $table";
+$result = $conn->query($sql);
+while ($row = $result->fetch_assoc()) {
+    $options[] = array("id" => $row[$idField], "description" => $row[$descField]);
+}
+
+echo json_encode($options);
+?>
